369b59ec4f17c1818020852abcb8ac94cd4e15b5
tekt <tekt@optexture.com>
2020-04-12 13:54:07 -0700

    implemented the function switcher
    still need to start using it in combine

--- RTK/functionSwitcher/functionSwitcher.py
+++ RTK/functionSwitcher/functionSwitcher.py
@@ -3,26 +3,41 @@ if False:
 	# noinspection PyUnresolvedReferences
 	from _stubs import *
 
-def _getModeParams(modes: 'DAT', row: int):
+def _getModeParams(modes: 'DAT', row: int, prefix: str = ''):
 	return [
-		'@' + modes[0, col].val
+		prefix + modes[0, col].val
 		for col in range(3, modes.numCols)
 		if modes[row, col] == '1'
 	]
 
-def buildOptimizedBody(modes: 'DAT', modeIndex: int, commonParams: 'list'):
-	params = list(commonParams or []) + _getModeParams(modes, modeIndex + 1)
-	return '\treturn {}({});'.format(modes[modeIndex + 1, 'function'].val, ','.join(params))
+def buildFunction(template: str):
+	modes = op('modes')
+	commonParams = parent().par.Commonparams.eval().split(' ')
 
-def buildUnoptimizedBody(modes: 'DAT', commonParams: 'list', modeParamName: str, defaultExpr: str):
-	commonParams = list(commonParams or [])
-	body = '\tswitch (int(@{})) {{\n'.format(modeParamName)
-	for row in range(1, modes.numRows):
-		params = commonParams + _getModeParams(modes, row)
-		body += '\t\tcase {}: return {}({});\n'.format(
-			row,
-			modes[row, 'function'].val,
-			','.join(params)
-		)
-	body += '\t\tdefault: return {};\n\t}}'.format(defaultExpr)
-	return body
+	if parent().par.Optimize:
+		modeIndex = int(parent().par.Modeindex)
+		params = list(commonParams or []) + _getModeParams(modes, modeIndex + 1, '@')
+		body = '\treturn {}({});'.format(modes[modeIndex + 1, 'function'].val, ','.join(params))
+	else:
+		modeParamName = parent().par.Modeparamname.eval()
+		defaultExpr = parent().par.Defaultexpr.eval()
+		body = '\tswitch (int(@{})) {{\n'.format(modeParamName)
+		for row in range(1, modes.numRows):
+			params = commonParams + _getModeParams(modes, row, '@')
+			body += '\t\tcase {}: return {}({});\n'.format(
+				row,
+				modes[row, 'function'].val,
+				','.join(params)
+			)
+		body += '\t\tdefault: return {};\n\t}}'.format(defaultExpr)
+	return template.replace('BODY', body)
+
+def getOpParamList():
+	modes = op('modes')
+	if parent().par.Optimize:
+		return _getModeParams(modes, parent().par.Modeindex + 1)
+	else:
+		return [parent().par.Modeparamname.eval()] + [
+			modes[0, col].val
+			for col in range(3, modes.numCols)
+		]
