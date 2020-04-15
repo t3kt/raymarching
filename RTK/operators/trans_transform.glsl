446fd40b31296a6a58f5b42aff90982219828ae0
tekt <tekt@optexture.com>
2020-04-14 19:58:26 -0700

    parameter order bug fix in transform

--- RTK/operators/trans_transform.glsl
+++ RTK/operators/trans_transform.glsl
@@ -1,9 +1,9 @@
 Sdf thismap(vec3 p, Context ctx) {
 	p = scaleRotateTranslate(
 		p,
-		vec3(@Scalex, @Scaley, @Scalez),
-		radians(vec3(@Rotatex,@Rotatey, @Rotatez)),
 		vec3(@Translatex, @Translatey, @Translatez),
+		radians(vec3(@Rotatex,@Rotatey, @Rotatez)),
+		vec3(@Scalex, @Scaley, @Scalez),
 		vec3(@Pivotx, @Pivoty, @Pivotz)
 	);
 	return inputOp1(p, ctx);
