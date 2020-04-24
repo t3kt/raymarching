abc008af9ac08ec2032c18a52f5f41c57b772fae
tekt <tekt@optexture.com>
2020-04-23 19:44:18 -0700

    drag and drop support for inspector

--- RTK/inspector/inspector.py
+++ RTK/inspector/inspector.py
@@ -19,3 +19,24 @@ def filterShaderCode(shaderCode, part):
 	if not match:
 		return ''
 	return match.group(1)
+
+
+# the drop function takes the following arguments and according to the dropped type
+# calls a function in the /sys attached DragDrop extension
+#  
+# dropName: dropped node name or filename
+# [x/y]Pos: position in network pane
+# index: index of dragged item
+# totalDragged: total amount of dragged items
+# dropExt: operator type or file extension of dragged item
+# baseName: dragged node parent network or parent directory of dragged file
+# destPath: dropped network
+
+def onDrop(dropName, xPos, yPos, index, totalDragged, dropExt, baseName, destPath):
+	print(parent().path, 'DROP ' + repr(locals()))
+	parentOp = op(baseName)
+	o = parentOp.op(dropName)
+	if o.isCOMP:
+		o = o.op('definition') or o.op('definition_out')
+	if o and o.isDAT:
+		parent().par.Definition = o
