b658adfc56c81c953a3225c5d201f778c9c10438
tekt <tekt@optexture.com>
2020-04-26 15:05:08 -0700

    adding bypass switches

--- RTK/operators/mat_checkerMat_material.glsl
+++ RTK/operators/mat_checkerMat_material.glsl
@@ -1,6 +1,6 @@
 // vec4 getMat(float m, vec3 pos, vec3 n, vec3 ref, vec3 eye, float occ, float occ2)		
 // vec4 col = vec4(vec3(0.5),1);
-col.rgb = vec3(@Colorx,@Colory,@Colorz);
+col.rgb = vec3(@Colorr,@Colorg,@Colorb);
 return col;
 
 	
--- RTK/operators/mat_reflectMat_material.glsl
+++ RTK/operators/mat_reflectMat_material.glsl
@@ -1,6 +1,6 @@
 // vec4 getMat(float m, vec3 pos, vec3 n, vec3 ref, vec3 refraction, vec3 eye, float occ, float occ2, float t, vec3 rd)		
 // vec4 col = vec4(vec3(0.5),1);
-col.rgb = vec3(@Colorx,@Colory,@Colorz);
+col.rgb = vec3(@Colorr,@Colorg,@Colorb);
 col.rgb+= texture(@texture, pos.xz*vec2(@Texscalex, @Texscaley)).rgb*@Texamount;
 vec3 lightPos = lights[0];
 float ks= 0.5;
