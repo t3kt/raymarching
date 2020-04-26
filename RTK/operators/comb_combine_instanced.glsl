14ce315d8c547e5ea4b22847dc2aa5009670ec41
tekt <tekt@optexture.com>
2020-04-25 17:42:57 -0700

    instancing improvements

--- RTK/operators/comb_combine_instanced.glsl
+++ RTK/operators/comb_combine_instanced.glsl
@@ -5,35 +5,13 @@ BODY
 ;
 	return res;
 }
-vec3 thismap__transform(vec3 p, Context ctx, int n) {
-	vec3 t = texelFetch(thismap_instances, ctx.iteration).rgb;
-	vec3 r = texelFetch(thismap_instances, ctx.iteration + n).rgb;
-	vec3 s = texelFetch(thismap_instances, ctx.iteration + n * 2).rgb;
-	vec3 pivot = texelFetch(thismap_instances, ctx.iteration + n * 3).rgb;
-//	vec3 t = texelFetch(thismap_instances_t, ctx.iteration).rgb;
-//	vec3 r = texelFetch(thismap_instances_r, ctx.iteration).rgb;
-//	vec3 s = texelFetch(thismap_instances_s, ctx.iteration).rgb;
-//	vec3 pivot = texelFetch(thismap_instances_p, ctx.iteration).rgb;
-	
-//	t *= 0;
-//	t+=vec3(0.4, 0, 0)*ctx.iteration;
-	//r *= 0;
-	//+vec3(50,0,0)*ctx.iteration)
-//	s = vec3(1);
-	return scaleRotateTranslate(p,
-		t,
-		radians(r),
-		1/s,
-		pivot
-	);
-}
 Sdf thismap(vec3 p, Context ctx) {
 	ctx.iteration = 0;
 	int n = int(@Instancecount);
-	Sdf res = inputOp1(thismap__transform(p, ctx, n), ctx);
+	Sdf res = inputOp1(p, ctx);
 	for (int i = 1; i < n; i++) {
 		ctx.iteration = i;
-		res = thismap__step(thismap__transform(p, ctx, n), ctx, res);
+		res = thismap__step(p, ctx, res);
 	}
 	return res;
 }
--- RTK/operators/trans_transform_instanced.glsl
+++ RTK/operators/trans_transform_instanced.glsl
@@ -1,14 +1,15 @@
 Sdf thismap(vec3 p, Context ctx) {
+	int n = int(@Instancecount);
 	vec3 t = texelFetch(thismap_instances, ctx.iteration).rgb;
 	vec3 r = texelFetch(thismap_instances, ctx.iteration + n).rgb;
 	vec3 s = texelFetch(thismap_instances, ctx.iteration + n * 2).rgb;
 	vec3 pivot = texelFetch(thismap_instances, ctx.iteration + n * 3).rgb;
 	p = scaleRotateTranslate(
 		p,
-		vec3(@Translatex, @Translatey, @Translatez),
-		radians(vec3(@Rotatex,@Rotatey, @Rotatez)),
-		vec3(1) / vec3(@Scalex, @Scaley, @Scalez),
-		vec3(@Pivotx, @Pivoty, @Pivotz)
+		t,
+		radians(r),
+		vec3(1) / s,
+		pivot
 	);
 	return inputOp1(p, ctx);
 }
