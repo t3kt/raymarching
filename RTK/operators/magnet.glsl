63f4837337412c8c0c017681310d8c9e2c0fc5bd
tekt <tekt@optexture.com>
2020-05-23 16:39:58 -0700

    added magnet op

---
+++ RTK/operators/magnet.glsl
@@ -0,0 +1,9 @@
+Sdf thismap(vec3 p, Context ctx) {
+	p -= THIS_Center;
+	float amount = smoothstep(0, THIS_Fade, length(p) - THIS_Radius);
+	p *= rotateMatrix(radians(mix(THIS_Rotate * THIS_Amount, vec3(0), amount)));
+	vec3 scale = mix(vec3(1), mix(THIS_Scale, vec3(1), amount), THIS_Amount);
+	Sdf res = inputOp1((p / scale) + THIS_Center, ctx);
+	res.x /= length(scale);
+	return res;
+}
