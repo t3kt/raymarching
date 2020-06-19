d39e03a7fcf2b26c175851dbde9b8882829ee238
tekt <tekt@optexture.com>
2020-06-18 20:56:17 -0700

    spin fix

--- RTK/operators/helix.glsl
+++ RTK/operators/helix.glsl
@@ -1,7 +1,6 @@
 Sdf thismap(vec3 p, Context ctx) {
-	p *= TDRotateOnAxis(radians(THIS_Spin), vec3(0, 1, 0));
 	return createSdf(sdHelix(
-		p - THIS_Translate,
+		(p - THIS_Translate) * TDRotateOnAxis(radians(THIS_Spin), vec3(0, 1, 0)),
 		THIS_Radius, THIS_Thickness, THIS_Spread,
 		THIS_Dualspread * THIS_Radius));
 }
