ef84167d29845ad83a485d6e8473f8a452610321
tekt <tekt@optexture.com>
2020-06-18 20:47:46 -0700

    add spin parameter in helix

--- RTK/operators/helix.glsl
+++ RTK/operators/helix.glsl
@@ -1,4 +1,5 @@
 Sdf thismap(vec3 p, Context ctx) {
+	p *= TDRotateOnAxis(radians(THIS_Spin), vec3(0, 1, 0));
 	return createSdf(sdHelix(
 		p - THIS_Translate,
 		THIS_Radius, THIS_Thickness, THIS_Spread,
