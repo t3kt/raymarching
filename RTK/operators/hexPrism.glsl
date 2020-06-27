f7df1b9d4ddb83335dd585e3802613698f45f0f3
tekt <tekt@optexture.com>
2020-06-27 16:25:52 -0700

    fix transform order issues in prism gens

--- RTK/operators/hexPrism.glsl
+++ RTK/operators/hexPrism.glsl
@@ -1,3 +1,3 @@
 Sdf thismap(vec3 p, Context ctx) {
-	return createSdf(sdHexPrism((p*TDRotateOnAxis(radians(THIS_Rotate), vec3(0, 0, 1))) - THIS_Translate, vec2(THIS_Radius, THIS_Height)));
+	return createSdf(sdHexPrism((p - THIS_Translate)*TDRotateOnAxis(radians(THIS_Rotate), vec3(0, 0, 1)), vec2(THIS_Radius, THIS_Height)));
 }
--- RTK/operators/triPrism.glsl
+++ RTK/operators/triPrism.glsl
@@ -1,3 +1,3 @@
 Sdf thismap(vec3 p, Context ctx) {
-	return createSdf(sdTriPrism((p*TDRotateOnAxis(radians(THIS_Rotate), vec3(0, 0, 1))) - THIS_Translate, vec2(THIS_Radius, THIS_Height)));
+	return createSdf(sdTriPrism((p - THIS_Translate)*TDRotateOnAxis(radians(THIS_Rotate), vec3(0, 0, 1)), vec2(THIS_Radius, THIS_Height)));
 }
