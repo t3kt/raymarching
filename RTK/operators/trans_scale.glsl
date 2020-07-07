e05d1f29d11d361cc05c2084454fd1605179a375
tekt <tekt@optexture.com>
2020-07-06 18:40:45 -0700

    more 2d support

--- RTK/operators/trans_scale.glsl
+++ RTK/operators/trans_scale.glsl
@@ -1,8 +1,5 @@
-Sdf thismap(vec3 p, Context ctx) {
-	// Not sure this is correct
-	vec3 scaleVec = THIS_Scale;
-	p /= scaleVec;
-	Sdf res = inputOp1(p, ctx);
-	res.x /= length(scaleVec);
+Sdf thismap(COORD p, Context ctx) {
+	Sdf res = inputOp1(p / THIS_Scale, ctx);
+	res.x /= length(THIS_Scale);
 	return res;
 }
