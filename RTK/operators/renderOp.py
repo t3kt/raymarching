7799e3b66bcaa287f00e521570c580a687055700
tekt <tekt@optexture.com>
2020-07-07 21:05:19 -0700

    fixing crashes
    The issue seems to be in /RTK/operators/render2d/shaderBuilder/dummy_definitionCore. The mere existence of that OP causes it to crash on load. So I locked the DAT that it outputs and deleted it. And for some reason that fixed things.

--- RTK/operators/renderOp.py
+++ RTK/operators/renderOp.py
@@ -7,14 +7,15 @@ def updateTextureInputs():
 	sources = op('texture_sources')
 	render = op('glsl_render')
 	depth = op('depth1')
-	depthConnected = False
-	for conn in render.inputConnectors:
-		if conn.connections and conn.connections[0].owner == depth:
-			depthConnected = True
-		else:
-			conn.disconnect()
-	if not depthConnected:
-		depth.outputConnectors[0].connect(render)
+	if depth:
+		depthConnected = False
+		for conn in render.inputConnectors:
+			if conn.connections and conn.connections[0].owner == depth:
+				depthConnected = True
+			else:
+				conn.disconnect()
+		if not depthConnected:
+			depth.outputConnectors[0].connect(render)
 	for i in range(sources.numRows):
 		texSelect = op(f'sel_texture_{i}')
 		if not texSelect:
