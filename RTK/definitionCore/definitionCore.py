364a5e0734bc8a1528348ddf1b0e579538796929
tekt <tekt@optexture.com>
2020-08-19 21:50:28 -0700

    much simpler implementation of local macros
    closes #16.
    it would be nice to be able to avoid having the gpu driver handle them, but at least it works and the code is syntactically valid

--- RTK/definitionCore/definitionCore.py
+++ RTK/definitionCore/definitionCore.py
@@ -1,8 +1,6 @@
 import re
 from typing import Dict, List, Union
 
-from pypreprocessor import Preprocessor
-
 # noinspection PyUnreachableCode
 if False:
 	# noinspection PyUnresolvedReferences
@@ -128,12 +126,12 @@ def prepareShaderCode(code: str):
 	if not code:
 		return ''
 	code = _stripLineComments(code)
-	code = _processLocalMacros(code)
 	# inject name
 	name = str(parent().par.Name)
 	code = code.replace('@', name + '_')
 	code = code.replace('THIS', name)
-	code = _spaceRx.sub(' ', code)
+	code = code.replace('thismap', name)
+	# code = _spaceRx.sub(' ', code)
 	return code
 
 def _stripLineComments(code: str):
@@ -148,49 +146,6 @@ def _stripLineComments(code: str):
 		else:
 			outLines.append(line.split('//', maxsplit=1)[0])
 	return '\n'.join(outLines)
-
-def _getLocalMacros():
-	par = parent().par['Localmacrotable']
-	if not par:
-		return []
-	table = par.eval()  # type: DAT
-	return [
-		(
-			row[0].val,
-			row[1].val if len(row) > 1 else '',
-		)
-		for row in table.rows()
-	]
-
-def _processLocalMacros(code: str):
-	'''
-	Handles code like this:
-	vec4 stuff() {
-	//-#ifdef USING_STUFF
-	return calculateStuff();
-	//-#else
-	return vec4(0);
-	//-#endif
-
-	then also replaces any occurrences of local macros in text (after preprocessing) with the
-	associated value
-	'''
-	if not code:
-		return ''
-	hasPreproc = _localMacroEscape in code
-	macros = _getLocalMacros()
-	if hasPreproc:
-		preproc = Preprocessor(
-			defines=[macro[0] for macro in macros],
-			removeMeta=True,
-			escape=_localMacroEscape,
-		)
-		code = preproc.parse(code)
-	if macros:
-		for macro in macros:
-			if macro[1]:
-				code = code.replace(macro[0], macro[1])
-	return code
 
 def buildDefinition(dat: 'DAT'):
 	dat.clear()
--- RTK/operators/magnet.glsl
+++ RTK/operators/magnet.glsl
@@ -1,6 +1,11 @@
 Sdf thismap(vec3 p, Context ctx) {
 	p -= THIS_Center;
-	float amount = smoothstep(0, THIS_Fade, length(p) - THIS_Radius);
+	#ifdef THIS_USE_INPUT_FIELD
+	float val = inputOp2(p - THIS_Center, ctx);
+	#else
+	float val = length(p);
+	#endif
+	float amount = smoothstep(0, THIS_Fade, val - THIS_Radius);
 	p *= rotateMatrix(radians(mix(THIS_Rotate * THIS_Amount, vec3(0), amount)));
 	vec3 scale = mix(vec3(1), mix(THIS_Scale, vec3(1), amount), THIS_Amount);
 	Sdf res = inputOp1((p / scale) + THIS_Center, ctx);
