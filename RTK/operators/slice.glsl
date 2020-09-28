567c95d7ed7fb5764b46aad40eddbed601817fc0
tekt <tekt@optexture.com>
2020-09-27 18:38:03 -0700

    add mirroring to slice

--- RTK/operators/slice.glsl
+++ RTK/operators/slice.glsl
@@ -1,6 +1,11 @@
 Sdf thismap(vec3 p, Context ctx) {
 	Sdf res = inputOp1(p, ctx);
-	float d = abs(p.AXIS - THIS_Offset) - THIS_Thickness;
+	#ifdef THIS_USE_MIRROR
+	float q = abs(p.AXIS);
+	#else
+	float q = p.AXIS;
+	#endif
+	float d = abs(q - THIS_Offset) - THIS_Thickness;
 	#ifdef THIS_USE_SMOOTHING
 	res.x = fOpIntersectionRound(res.x, d, THIS_Smoothradius);
 	#else
