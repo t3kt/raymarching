d37811fa53de0daec703ea4326c61a55184daa72
tekt <tekt@optexture.com>
2020-09-27 18:20:46 -0700

    fix bugs in flip

--- RTK/operators/flip.glsl
+++ RTK/operators/flip.glsl
@@ -3,5 +3,15 @@ Sdf thismap(vec3 p, Context ctx) {
 	q.AXIS += THIS_Offset;
 	q.AXIS *= -1;
 	q.AXIS -= THIS_Shift;
+	#if defined(THIS_MERGE_none)
 	return inputOp1(q, ctx);
+	#else
+		Sdf res1 = inputOp1(p, ctx);
+		Sdf res2 = inputOp1(q, ctx);
+		#ifdef THIS_MERGE_smoothUnion
+		return comb_smoothUnion(res1, res2, THIS_Mergeradius);
+		#else
+		return comb_simpleUnion(res1, res2);
+		#endif
+	#endif
 }
